4ab9667da63c5df89d3850c0f3726a2753ddca2a
EC2 Default User <ec2-user@ip-172-31-18-194.ap-south-1.compute.internal>
2020-12-17 13:53:26 +0000

    News Letter Component Added in App Comp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 
 function App() {
   return (
-    <div class="grids_bottom">
-    <div class="style-grids">
-      <div class="col-md-6 style-grid style-grid-1">
-        <img src="images/b1.jpg" alt="shoe">
-      </div>
-    </div>
-    <div class="col-md-6 style-grid style-grid-2">
-      <div class="style-image-1_info">
-        <div class="style-grid-2-text_info">
-          <h3>Nike DOWNSHIFTER</h3>
-          <p>Itaque earum rerum hic tenetur a sapiente delectus reiciendis maiores alias consequatur.sed quia non numquam eius modi
-            tempora incidunt ut labore et dolore .</p>
-          <div class="shop-button">
-            <a href="shop.html">Shop Now</a>
-          </div>
+    <div class="newsletter_w3layouts_agile">
+        <div class="col-sm-6 newsleft">
+            <h3>Sign up for Newsletter !</h3>
         </div>
-      </div>
-      <div class="style-image-2">
-        <img src="images/b2.jpg" alt="shoe">
-        <div class="style-grid-2-text">
-          <h3>Air force</h3>
+        <div class="col-sm-6 newsright">
+            <form action="#" method="post">
+                <input type="email" placeholder="Enter your email..." name="email" required="">
+                <input type="submit" value="Submit">
+            </form>
         </div>
-      </div>
+
+        <div class="clearfix"></div>
     </div>
-    <div class="clearfix"></div>
-  </div>
   );
 }
 
