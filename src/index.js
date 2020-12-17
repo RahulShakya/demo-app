d1627b6e7dc131c61985f139b665f5ded11324e2
EC2 Default User <ec2-user@ip-172-31-18-194.ap-south-1.compute.internal>
2020-12-17 12:16:23 +0000

    Add Routing

--- src/index.js
+++ src/index.js
@@ -11,6 +11,31 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// More components
+class Login extends Component {
+    render(){
+        return (<h1>Cars page</h1>);
+    }
+}
+
+
+class Register extends Component {
+    render(){
+        return (<h1>Cars page</h1>);
+    }
+}
+
+
+
+render(
+    <Router>
+        <Route path="/home" component={Home}/>
+        <Route path="/login" component={Login}/>
+        <Route path="/register" component={Register}/>
+    </Router>,
+    document.getElementById('root')
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
